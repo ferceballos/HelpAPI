6075f2f668e1f053780429531ace2b42e75ce55b
KuroiPepe <alfaceta_antonio@hotmail.com>
2017-10-22 03:28:34 -0500

    Agregar​ ​usuario
    ya se puede "agregar usuarios"

--- helpAPI/routes/users.js
+++ helpAPI/routes/users.js
@@ -20,16 +20,30 @@ var MyAppModel = mysqlModel.createConnection({
 router.get('/login/:mail/:pwd', function(req, res, next) {
   var mail = req.params.mail;
   var pwd = req.params.pwd;
-  var respuesta; 
-
   //Aqui es donde estoy haciendo las pruebas a ver si agarro algo
   user.query("SELECT US_Nombre FROM usuarios WHERE US_Correo='"+mail+"' and US_Pass='"+pwd+"';", function(err, rows){
       if(rows=="")
-      res.send("Datos incorrectos");
+        res.send("Datos incorrectos");
       else
-      res.send(rows);
+        res.send(rows);
   });
 });
+
+router.get('/signup/:name/:mail/:pwd/:rol/:dep', function(req, res, next) {
+  var mail = req.params.mail;
+  var pwd = req.params.pwd;
+  var dep =req.params.dep;
+  var rol = req.params.rol;
+  var name = req.params.name;
+  user.query("INSERT into usuarios (US_Nombre,US_Correo,US_Pass,US_Rol,US_Dependencia) VALUES ('"+name+"','"+mail+"','"+pwd+"',"+rol+","+dep+");", function(err, callback){
+    if(callback!=null) 
+      res.send("Registro Correcto");
+    else
+      res.send("Registro Incorrecto");
+  });
+});
+
+
 
 
 module.exports = router;
