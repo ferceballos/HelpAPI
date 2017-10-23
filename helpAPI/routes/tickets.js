17fa382733cf97d4a94803c77ce45f6c847daadf
KuroiPepe <alfaceta_antonio@hotmail.com>
2017-10-23 17:52:38 -0500

    empezando tickets
    todavia falta comprobar que los querys den el resultado esperado

--- helpAPI/routes/tickets.js
+++ helpAPI/routes/tickets.js
@@ -18,6 +18,34 @@ var MyAppModel = mysqlModel.createConnection({
 
 //Direcciones y metodos empiezan aqui
 
+//Obtener​ ​un​ ​ticket
+router.get('/getbyfolio/:folio', function(req, res, next) {
+  var folio = req.params.folio;
+  //Regresa todos los datos del ticket con el folio solicitado 
+  ticket.query("SELECT * FROM tickets WHERE TI_Folio="+folio+";", function(err, rows){
+    res.send(rows);
+  })
+});
+
+//Obtener​ ​todos​ ​los​ ​tickets​ ​de​ ​un​ ​usuario
+router.get('/getbyuser/:user', function(req, res, next) {
+  var user = req.params.user;
+  //Regresa todos los datos de todos los tickets que tenga un usuario 
+  ticket.query("SELECT * FROM tickets WHERE TI_Usuario_Solicitante='"+user+"';", function(err, rows){
+    res.send(rows);
+  })
+});
+
+//Obtener​ ​todos​ ​los​ ​tickets​ ​asignados​ ​a​ ​un​ ​bibliotecario
+router.get('/getbylibrarian/:user', function(req, res, next) {
+  var user = req.params.user;
+  //Regresa todo los datos del ticket que estan asignados a un bibliotecario 
+  //Todavia no esta terminado y todas las demas todavia faltan de revision 
+  ticket.query("SELECT * FROM tickets, usuarios WHERE TI_Usuario_Solicitante='"+user+"' AND US_Rol=3", function(err, rows){
+    res.send(rows);
+  })
+});
+
 router.get('/', function(req, res, next) {
   res.send('Here are the tickets methods')
 });
